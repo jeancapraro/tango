f405c672b4b98c741633d2d8b6271f96c6694363
Beatriz Marques <beatrizmaarqueszo@gmail.com>
2017-10-18 10:47:48 -0200

    Página About

--- rango/views.py
+++ rango/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Oi!"}
+    return render(request, 'rango/about.html', context=context_dict)
